ca4ad27789a393a1b47bf6395449e524deefb5ad
Diel09 <eliezer14galano@gmail.com>
2023-10-04 14:10:46 +0800

    beautiful login

---
+++ resources/js/Components/PageFooter.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { HeartIcon } from '@heroicons/vue/solid'
+</script>
+
+<template>
+    <footer class="flex-shrink-0 px-6 py-4">
+        <p class="flex items-center justify-center gap-1 text-sm text-gray-600  dark:text-gray-400">
+            Sample Footer
+        </p>
+    </footer>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +17,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/components/buttons', function () {
+    return Inertia::render('Components/Buttons');
+})->middleware(['auth', 'verified'])->name('components.buttons');
+
+
+require __DIR__ . '/auth.php';
